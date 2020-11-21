d6c05757422372cfe5230eec4f8765feabbee60e
tom <tom@sapletta.com>
2020-11-21 20:19:17 +0100

    update update server

--- restart.sh
+++ restart.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-./status.sh & ./stop.sh & ./update.sh & ./start.sh & ./status.sh && echo "Successfully restarted"
+./status.sh
+./stop.sh
+./update.sh
+./start.sh
+./status.sh && echo "Successfully restarted"
