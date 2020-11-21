f2b8f5cec87ab22289e7d953b577c393482a872c
tom <tom@sapletta.com>
2020-11-21 19:07:38 +0100

    update for scripts

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express')
 var app = express()
-var wildcardSubdomains = require('../')
+var wildcardSubdomains = require('wildcard-subdomains')
 
 app.use(wildcardSubdomains())
 
 app.get('/', function (req, res) {
-    res.send('Visit <a href="http://foo.vcap.me:5555">http://foo.vcap.me:5555</a> (points back to localhost)')
+    res.send('Visit <a href="http://foo.apipot.com:80">http://foo.apipot.com:80</a> (points back to localhost)')
 })
 
 app.get('/_sub/bar/foo', function (req, res) {
@@ -41,9 +41,9 @@ app.get('/_sub/:firstSubdomain/*', function (req, res) {
     )
 })
 
-app.listen(5555)
-console.log('Example started on port 5555')
+app.listen(80)
+console.log('Example started on port 80')
 console.log('============================')
 console.log('To test subdomain routing, visit these urls in your browser:')
-console.log('http://test.vcap.me:5555/')
-console.log('http://another.test.vcap.me:5555/')
+console.log('http://test.apipot.com:80/')
+console.log('http://another.test.apipot.com:80/')
