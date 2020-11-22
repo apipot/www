9a43d1e6ed7c53698aba02377fdb88535746c1d4
tom <tom@sapletta.com>
2020-11-22 13:14:52 +0100

    preparation to processing files from data

--- data.sh
+++ data.sh
@@ -9,22 +9,24 @@ ls data/in/
 # create folder for groups and repositories by file list in data folder
 for f in data/in/*.txt; do
   SAMPLE=`basename ${f%%.*}`;
+  OUTPUT="data/out/${SAMPLE}.txt";
   #echo $SAMPLE;
-  if [ ! -f data/out/${SAMPLE}.txt ]; then
+  if [ ! -f OUTPUT ]; then
      echo ":: processing: ${SAMPLE}";
      #cat `data/in/${SAMPLE}.txt` | while read LINE; do echo "--${LINE}--"; done
      #same loop over files, but using a pipe (reading from standard input), and a while-loop
      #ls `data/in/${SAMPLE}.txt` | while read LINE; do echo "--${LINE}--"; done;
     # Set the filename with path
     INPUT="data/in/${SAMPLE}.txt";
-    #while IFS= read -r line; do \
-    while IFS= read -r line || [[ -n "$line" ]]; do \
-      #echo "-- $line --";
-      # Read the filename without extension by using ‘basname’ and `sed` command
-      FILE="$(basename "$line" | sed 's/\(.*\)\..*/\1/')"; \
-      PATH="${REPO}/${ENV}/${SAMPLE}/${FILE}"; \
-      git clone $line $REPO; \
-    done < "$INPUT";
+    # check if is existing
+    if [ -f INPUT ]; then \
+      while IFS= read -r line || [[ -n "$line" ]]; do \
+        #echo "-- $line --";
+        # Read the filename without extension by using ‘basname’ and `sed` command
+        FILE="$(basename "$line" | sed 's/\(.*\)\..*/\1/')"; \
+        PATH="${REPO}/${ENV}/${SAMPLE}/${FILE}"; \
+        git clone $line $REPO; \
+      done < "$INPUT";
 
      #mv data/in/${SAMPLE}.txt data/out/${SAMPLE}.txt;
   fi;
