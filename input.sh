99f1a70e0ecaa58ce724d01971972373bb498f38
tom <tom@sapletta.com>
2020-11-22 13:48:41 +0100

    preparation to processing files from data

--- input.sh
+++ input.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "I am loading example files"
-for f in data/in/*.txt do
+INPUT_LIST="data/in/*.txt";
+for f in $INPUT_LIST; do
+#for f in data/in/*.txt; do
   FILE=${f%%.*};
   echo $FILE;
 
