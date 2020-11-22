6ffbbf6e08415c9247e6ca2d75268824f43dfe98
tom <tom@sapletta.com>
2020-11-22 21:04:23 +0100

    ssl

--- ssl_install.sh
+++ ssl_install.sh
@@ -5,5 +5,5 @@ sudo apt-get install certbot
 certbot certonly --manual
 # 1. Type your domain name(s) without the protocol part. For instance: yourdomain.com or even muchdomain.verysite.
 # 2. Type Y then ENTER.
-#/.well-known/acme-challenge/a-string
+#/.well-known/acme-challenge/_rL5K-S01wz3x_Q3AHtsO2-I4IZSLP-L72dcaloII_A
 # http://apipot.com/.well-known/acme-challenge/a-string
--- test.sh
+++ test.sh
@@ -18,4 +18,6 @@ auth.apipot.com
 user.auth.apipot.com
 admin.auth.apipot.com
 
-apipot.com,www.apipot.com,input.apipot.com,radio.input.apipot.com,checkbox.input.apipot.com,view.apipot.com,list.view.apipot.com,grid.view.apipot.com,auth.apipot.com,user.auth.apipot.com,admin.auth.apipot.com
+apipot.com,input.apipot.com,radio.input.apipot.com,checkbox.input.apipot.com,view.apipot.com,list.view.apipot.com,grid.view.apipot.com,auth.apipot.com,user.auth.apipot.com,admin.auth.apipot.com
+
+ls server/.well-known/acme-challenge/
